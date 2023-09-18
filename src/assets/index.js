6e8f85f66b09c66b6715f55d4775d67733a804be
Palak <Palakgoyal0304@gmail.com>
2023-09-18 22:57:58 +0530

    Initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import logo_black from "./BlogZen-logos_black.png"
+import logo_transparent from "./BlogZen-logos_transparent.png"
+import logo_white from "./BlogZen-logos_white.png"
+import logo from "./BlogZen-logos.jpeg"
+
+export {
+    logo_black,
+    logo_transparent,
+    logo_white,
+    logo,
+}
